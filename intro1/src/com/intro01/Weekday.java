d22b3762037d20ef3b3c748cbad885d039609a3f
yang <diddnjswns20@gmail.com>
2023-07-31 16:40:23 +0900

    test

---
+++ intro1/src/com/intro01/Weekday.java
@@ -0,0 +1,5 @@
+package com.intro01;
+
+public enum Weekday {
+	MON, TUE, WEN, THUR, FRI, SAT, SUN;
+}
---
+++ intro1/src/com/intro01/switchcase.java
@@ -0,0 +1,24 @@
+package com.intro01;
+
+import java.util.Scanner;
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		System.out.println("what num?");
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+//
+//		
+//		switch(num%2) {
+//		case 0:
+//			System.out.println("even");
+//			break;
+//		case 1:
+//			System.out.println("odd");
+//			break;
+		}
+		
+	
+}
